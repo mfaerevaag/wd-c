99338b1f57ae8d422c0674a117ba865cb870a753
Markus Faerevaag <m@frv.ag>
2016-07-28 02:10:46 +0200

    add arg parsing

--- wd.c
+++ wd.c
@@ -1,40 +1,94 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <getopt.h>
+#include <string.h>
 
-char *RC_FILE = "/home/markus/.warprc";
+static int QUIET_FLAG;
+/* static char *RC_FILE = "~/.warprc"; */
+static int ARGC = 0;
+static char **ARGV = NULL;
 
-int read_rc()
-{
-    FILE *fp;
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t read;
-    int count = 0;
+int parse_args(int argc, char **argv);
 
-    /* open file */
-    fp = fopen(RC_FILE, "r");
-    if (fp == NULL)
-        return(1);
-
-    /* read file */
-    while ((read = getline(&line, &len, fp)) != -1) {
-        printf("%2d[%2zu]: %s", count, read, line);
-        count++;
-    }
-
-    /* free */
-    fclose(fp);
-    if (line)
-        free(line);
-
-    return 0;
-}
-
-int main()
+int main(int argc, char **argv)
 {
     int rc;
 
-    rc = read_rc();
+    /* rc = read_rc(); */
+
+    rc = parse_args(argc, argv);
 
     return rc;
+}
+
+int parse_args(int argc, char **argv)
+{
+    int c, ret;
+    ret = 0; /* all good so far */
+    opterr = 1; /* prevent error messages if 0 */
+
+    while(1)
+    {
+        int option_index = 0;
+
+        static struct option long_options[] =
+            {
+                {"quiet",   no_argument,       &QUIET_FLAG, 1},
+                {"version", no_argument,       0,  0 },
+                {"help",    no_argument,       0, 'h'},
+                {"config",  required_argument, 0, 'c'},
+                {0, 0, 0, 0}
+            };
+
+        c = getopt_long(argc, argv, "hc:",
+                        long_options, &option_index);
+
+        if (c == -1)
+            break;
+
+        switch (c)
+        {
+        case 0:
+            printf("TODO: version\n");
+            exit(0); // TODO: ret code
+            break;
+
+        case 'h':
+            printf("TODO: wow, such help\n");
+            exit(0); // TODO: ret code
+            break;
+
+        case 'c':
+            // TODO: config file
+            printf("TODO: config file `%s'\n", optarg);
+            break;
+
+        case '?':
+            // TODO: logging
+            break;
+
+        default:
+            abort();
+        }
+    }
+
+    /* store rest arguments */
+    if (optind < argc) {
+        ARGV = calloc(sizeof(char*), argc - optind);
+
+        char *opt;
+        while (optind < argc) {
+            opt = argv[optind++];
+            printf("opt: %s\n", opt); // TODO: debug
+            size_t size = strlen(opt);
+
+            ARGV[ARGC] = malloc(size);
+            memcpy((char *) ARGV[ARGC], (char *) opt, size);
+
+            ARGC++;
+        }
+    }
+
+    return ret;
 }
