1f71c382c026c0aec87a785a0cba83d19a66ba2c
Markus Faerevaag <m@frv.ag>
2017-02-10 19:15:17 +0900

    fix install errors on ubuntu

--- config.mk
+++ config.mk
@@ -2,8 +2,7 @@ CC ?= gcc
 
 CFLAGS ?= -W -pedantic -Werror -Wall -std=gnu99 \
 -fno-strict-aliasing -fno-common -Wno-unused-parameter \
--fstrict-aliasing -fstrict-overflow -fdiagnostics-color=always \
--Wno-return-local-addr
+-fstrict-aliasing -fstrict-overflow -Wno-return-local-addr
 
 prefix ?= /usr
 
