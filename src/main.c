50233da27c1c85cfffc31fb27d06efad9b2b41f8
Markus Faerevaag <m@frv.ag>
2017-09-26 13:56:42 +0900

    Add support for cd'ing to subdirectory on warp

--- src/main.c
+++ src/main.c
@@ -16,6 +16,9 @@ int DO_WARP = 1;
 
 int main(int argc, char **argv)
 {
+    int i, slash;
+    char *name, *path;
+
     /* parse args */
     if (parse_args(argc, argv) != 0) {
         log_err("failed to parse args");
@@ -26,21 +29,42 @@ int main(int argc, char **argv)
     if (DO_WARP == 1) {
         debug("do warp");
 
-        if (ARGC == 1) {
-            /* TODO: clean args, buffer overflow */
-            char *path = wd_path(ARGV[0]);
+        if(ARGC > 1) {
+            log_warn("cannot warp to multiple points");
+            exit(EXIT_ERROR);
+        } else if(ARGC < 1) {
+            log_warn("no warp point given");
+            exit(EXIT_ERROR);
+        }
 
-            if (path == NULL) {
-                log_warnf("no warp point named '%s'\n", ARGV[0]);
+        /* TODO: clean args, buffer overflow */
+        name = ARGV[0];
+
+        /* extract name from input */
+        slash = -1;
+        for (i = 0; i < strlen(name); ++i) {
+            if (name[i] == '/') {
+                slash = i;
+
+                /* end name string */
+                name[i] = '\0';
+            }
+        }
+
+        /* find path to point */
+        path = wd_path(name);
+
+        /* print warp path */
+        if (path == NULL) {
+            log_warnf("no warp point named '%s'\n", ARGV[0]);
+        } else {
+            /* check if slash is found */
+            if (slash > 0) { /* TODO: check for slash in input */
+                printf("%s/%s", path, &(name[slash + 1]));
             } else {
                 printf("%s", path);
             }
-        } else if(ARGC > 1) {
-            log_warn("cannot warp to multiple points");
-            exit(EXIT_ERROR);
-        } else {
-            log_warn("no warp point given");
-            exit(EXIT_ERROR);
+
         }
     }
 
@@ -56,106 +80,106 @@ int parse_args(int argc, char **argv)
     opterr = 1; /* prevent error messages if 0 */
 
     while (1)
-    {
-        int option_index = 0;
-
-        static struct option long_options[] =
-            {
-                {"quiet",   no_argument,       &QUIET_FLAG, 1},
-                {"version", no_argument,       0,  0 },
-                {"help",    no_argument,       0, 'h'},
-                {"list",    no_argument,       0, 'l'},
-                {"show",    no_argument,       0, 's'},
-                {"config",  required_argument, 0, 'c'},
-                {"remove",  required_argument, 0, 'r'},
-                {"add",     required_argument, 0, 'a'},
-                {"path",    required_argument, 0, 'p'},
-                {0, 0, 0, 0}
-            };
-
-        c = getopt_long(argc, argv, "hlsc:r:a:p:",
-                        long_options, &option_index);
-
-        if (c == -1)
-            break;
-
-        switch (c)
         {
-        case 0:
-            print_version();
-            exit(EXIT_INFO);
-            break;
+            int option_index = 0;
 
-        case 'h':
-            print_help();
-            exit(EXIT_INFO);
-            break;
+            static struct option long_options[] =
+                {
+                    {"quiet",   no_argument,       &QUIET_FLAG, 1},
+                    {"version", no_argument,       0,  0 },
+                    {"help",    no_argument,       0, 'h'},
+                    {"list",    no_argument,       0, 'l'},
+                    {"show",    no_argument,       0, 's'},
+                    {"config",  required_argument, 0, 'c'},
+                    {"remove",  required_argument, 0, 'r'},
+                    {"add",     required_argument, 0, 'a'},
+                    {"path",    required_argument, 0, 'p'},
+                    {0, 0, 0, 0}
+                };
 
-        case 'c':
-            debugf("setting rc file to '%s'\n", optarg);
-            rc_set_file(optarg);
-            break;
+            c = getopt_long(argc, argv, "hlsc:r:a:p:",
+                            long_options, &option_index);
 
-        case 'a':
-            debug("do add");
+            if (c == -1)
+                break;
 
-            /* TODO: clean args, env */
-            wd_add(optarg, getenv("PWD"));
-            DO_WARP = 0;
-            break;
+            switch (c)
+                {
+                case 0:
+                    print_version();
+                    exit(EXIT_INFO);
+                    break;
 
-        case 'r':
-            debug("do remove");
+                case 'h':
+                    print_help();
+                    exit(EXIT_INFO);
+                    break;
 
-            wd_remove(optarg);
-            DO_WARP = 0;
-            break;
+                case 'c':
+                    debugf("setting rc file to '%s'\n", optarg);
+                    rc_set_file(optarg);
+                    break;
 
-        case 's':
-            debug("do show");
+                case 'a':
+                    debug("do add");
 
-            char *pwd = getenv("PWD");
-            char *name = wd_show(pwd);
+                    /* TODO: clean args, env */
+                    wd_add(optarg, getenv("PWD"));
+                    DO_WARP = 0;
+                    break;
 
-            if (name == NULL) {
-                log_warn("no warp point to current dir");
-            } else {
-                printf("\t%10s -> %10s\n", name, pwd);
-            }
+                case 'r':
+                    debug("do remove");
 
-            DO_WARP = 0;
-            break;
+                    wd_remove(optarg);
+                    DO_WARP = 0;
+                    break;
 
-        case 'p':
-            debug("do path");
+                case 's':
+                    debug("do show");
 
-            /* TODO: clean arg */
-            char *path = wd_path(optarg);
+                    char *pwd = getenv("PWD");
+                    char *name = wd_show(pwd);
 
-            if (path == NULL) {
-                log_warnf("no warp point named '%s'\n", optarg);
-            } else {
-                printf("%s\n", path);
-            }
+                    if (name == NULL) {
+                        log_warn("no warp point to current dir");
+                    } else {
+                        printf("\t%10s -> %10s\n", name, pwd);
+                    }
 
-            DO_WARP = 0;
-            break;
+                    DO_WARP = 0;
+                    break;
 
-        case 'l':
-            debug("do list");
+                case 'p':
+                    debug("do path");
 
-            wd_print_all();
-            DO_WARP = 0;
-            break;
+                    /* TODO: clean arg */
+                    char *path = wd_path(optarg);
 
-        case '?':
-            // TODO: logging
-            break;
+                    if (path == NULL) {
+                        log_warnf("no warp point named '%s'\n", optarg);
+                    } else {
+                        printf("%s\n", path);
+                    }
 
-        default:
-            exit(EXIT_ERROR);
+                    DO_WARP = 0;
+                    break;
+
+                case 'l':
+                    debug("do list");
+
+                    wd_print_all();
+                    DO_WARP = 0;
+                    break;
+
+                case '?':
+                    // TODO: logging
+                    break;
+
+                default:
+                    exit(EXIT_ERROR);
+                }
         }
-    }
 
     /* store rest arguments */
     if (optind < argc) {
@@ -180,32 +204,32 @@ int parse_args(int argc, char **argv)
 void print_help()
 {
     printf(
-        "Usage: wd [command] <point>\n"
-        "\n"
-        "Example:\n"
-        "\t$ wd --add foo \t\t# points to current dir\n"
-        "\t$ cd ~/far/far/away\n"
-        "\t$ wd foo \t\t# and you're back!\n"
-        "\n"
-        "Commands:\n"
-        "--add <point>\tAdds the current working directory to your warp points\n"
-        /* "--add! <point>\tOverwrites existing warp point\n" */
-        "--remove <point>\tRemoves the given warp point\n"
-        "--list\t\tPrint all stored warp points\n"
-        "--show\t\tPrint warp points to current directory\n"
-        "--path <point>\tShow the path to given warp point\n"
-        /* "--ls  <point>\tShow files from given warp point\n" */
-        /* "--clean!\tRemove points warping to nonexistent directories\n" */
-        "\n"
-        "Other:\n"
-        "-h, --help\tShow this extremely helpful text\n"
-        "-v, --version\tPrint version\n"
-        "-c, --config <file>\tSpecify config file (default ~/.warprc)\n"
-        "-q, --quiet\tSuppress all output\n"
-        "\n"
-        "Licenced under MIT\n" // TODO
-        "Report bugs to github.com/mfaerevaag/wdc\n" // TODO
-        );
+           "Usage: wd [command] <point>\n"
+           "\n"
+           "Example:\n"
+           "\t$ wd --add foo \t\t# points to current dir\n"
+           "\t$ cd ~/far/far/away\n"
+           "\t$ wd foo \t\t# and you're back!\n"
+           "\n"
+           "Commands:\n"
+           "--add <point>\tAdds the current working directory to your warp points\n"
+           /* "--add! <point>\tOverwrites existing warp point\n" */
+           "--remove <point>\tRemoves the given warp point\n"
+           "--list\t\tPrint all stored warp points\n"
+           "--show\t\tPrint warp points to current directory\n"
+           "--path <point>\tShow the path to given warp point\n"
+           /* "--ls  <point>\tShow files from given warp point\n" */
+           /* "--clean!\tRemove points warping to nonexistent directories\n" */
+           "\n"
+           "Other:\n"
+           "-h, --help\tShow this extremely helpful text\n"
+           "-v, --version\tPrint version\n"
+           "-c, --config <file>\tSpecify config file (default ~/.warprc)\n"
+           "-q, --quiet\tSuppress all output\n"
+           "\n"
+           "Licenced under MIT\n" // TODO
+           "Report bugs to github.com/mfaerevaag/wdc\n" // TODO
+           );
 }
 
 void print_version()
